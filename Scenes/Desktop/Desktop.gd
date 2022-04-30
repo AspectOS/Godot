f2de901ee33a6710189c0def5f6762008ae9ff52
Kkidslogin <96443213+TheMowgliMan@users.noreply.github.com>
2022-04-30 16:33:56 -0400

    [ADD] Add initial files.

---
+++ Scenes/Desktop/Desktop.gd
@@ -0,0 +1,26 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_metrics()
+
+
+func screen_metrics():
+	print("                 [Screen Metrics]")
+	print("            Display size: ", OS.get_screen_size())
+	print("   Decorated Window size: ", OS.get_real_window_size())
+	print("             Window size: ", OS.get_window_size())
+	print("        Project Settings: Width=", ProjectSettings.get_setting("display/window/size/width"), " Height=", ProjectSettings.get_setting("display/window/size/height")) 
+	print(OS.get_window_size().x)
+	print(OS.get_window_size().y)
+
+
+func _on_Button_pressed():
+	print("exit")
+	get_tree().notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)
