9720f49f97e504c91bc9538daca3e2c1f6f828ab
Kkidslogin <96443213+TheMowgliMan@users.noreply.github.com>
2022-05-01 09:40:18 -0400

    [ADD] Add login sound

--- Scenes/Login/Login.gd
+++ Scenes/Login/Login.gd
@@ -4,5 +4,6 @@ extends CanvasLayer
 onready var desktop_scene = "res://Scenes/Desktop/Desktop.tscn"
 
 
-func _on_Guest_pressed():
+
+func _on_GuestMode_pressed():
 	get_tree().change_scene(desktop_scene)
